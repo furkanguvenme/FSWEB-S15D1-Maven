71b41226b74ad94739345fed85e75a2b89d46931
furkanguvenme <furkanguven.me@gmail.com>
2024-08-05 15:17:07 +0300

    Task Completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.models.Grocery;
+
 public class Main {
     public static void main(String[] args) {
-
+        Grocery.groceryList.add("tomato");
+        Grocery.addItems("lemon");
+        Grocery.addItems("cherry,artichoke");
+        System.out.println(Grocery.groceryList);;
     }
 }
